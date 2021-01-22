6d666e68215ec6fcadd52ef6b96fdef6cbb31216
Adnan-Pranto <prantoshikdar02@gmail.com>
2021-01-22 22:52:17 +0600

    All codes

--- assignment.js
+++ assignment.js
@@ -1,5 +1,7 @@
-    /* First Problem
-    Convert Kilometer To Meter */
+    https://github.com/Adnan-Pranto/Assignment_3
+        
+        /* First Problem
+            Convert Kilometer To Meter */
 
 function kilometerToMeter(kilometer) {
     var meter = kilometer * 1000;
@@ -10,15 +12,13 @@ function kilometerToMeter(kilometer) {
         return meter;
     }
 }
-var output = kilometerToMeter(10)
-console.log(output);
 
 
-     //Secound Problem : Budget Calculator 
+        //Secound Problem : Budget Calculator 
 
 function budgetCalculator(watch, mobile, laptop) {
     if (watch < 0 || mobile < 0 || laptop < 0) {
-        return " can't be negative";
+        return "Money can't be negative value..!!";
     }
     var watchPrice = 50 * watch;
     var mobilePrice = 100 * mobile;
@@ -28,24 +28,44 @@ function budgetCalculator(watch, mobile, laptop) {
     return total;
 
 }
-var total = budgetCalculator(4, 2, 1);
-console.log(total);
+
+            //Third Problem : Hotel Offers.. 
+
+
+function hotelCost(stay) {
+    var first_Offrer = 0;
+    var second_Offrer = 0;
+    var third_Offrer = 0;
+    var total_Cost = 0;
+
+    if (stay <= 0) {
+        return "Day's can't be zero or negative..!!"
+    }
+
+    for (var i = 1; i <= stay; i++) {
+        if (i >= 1 && i <= 10) {
+            first_Offrer = i * 100;
+        }
+        else if (i >= 11 && i <= 20) {
+
+            second_Offrer = i * 80 - 800;
+        }
+        else if (i >= 21) {
+            third_Offrer = i * 50 - first_Offrer;
+        }
+    }
+    total_Cost = first_Offrer + second_Offrer + third_Offrer;
+    return total_Cost;
+}
 
         //Fourth Problem : Largest String.. 
 
 function megaFriend(friends) {
     for (var i = 1; i < friends.length; i++) {
         var element = friends[i];
-
-
         if (element.length > maxWord.length) {
             maxWord = element;
         }
     }
     return maxWord;
 }
-//Declare Array for string.
-var friends = ['Adnan Rahman', 'Pranto', 'Rishad Amin', 'Ashiqur Rahman', 'Hamidul Islam'];
-var maxWord = friends[0];
-var biggestName = megaFriend(friends);
-console.log(biggestName);
